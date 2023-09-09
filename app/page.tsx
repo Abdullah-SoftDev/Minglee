01548db75630f68f9bbd2e1d437fbb743a744027
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-09-09 11:15:19 +0530

    Make the onBoarding route

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+import { redirect } from "next/navigation";
+
 const HomePage = async () => {
+  const userInfo = false;
+  if (!userInfo) redirect("/onBoarding");
+
   return (
     <>
       <h1 className="text-white font-medium text-2xl">Home</h1>
